7cdd4d7180f772a2842ea93d7589120cf82ec0de
ignatiusong <e0324134@u.nus.edu>
2020-03-02 15:45:18 +0800

    at this point I dont even know anymore

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -62,6 +62,13 @@ public class EditPersonDescriptorBuilder {
     }
 
     /**
+     * Sets the {@code Address} of the {@code EditPersonDescriptor} that we are building.
+     */
+    public EditPersonDescriptorBuilder withAddress(String address) {
+        return this;
+    }
+
+    /**
      * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code EditPersonDescriptor}
      * that we are building.
      */
